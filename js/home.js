94f71aa96e525c97a9909fda67e713ebd4a573ce
N1ghtHunter <93228608+N1ghtHunter@users.noreply.github.com>
2022-04-25 20:16:56 +0200

    v1.1

--- js/home.js
+++ js/home.js
@@ -1,33 +1,68 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-const menuToggle = document.querySelector('.menuToggle');
-menuToggle.addEventListener("click", function() {
-    menuToggle.classList.toggle('active')
-    openNav();
-  });
+const menuToggle = document.querySelector(".menuToggle");
+menuToggle.addEventListener("click", function () {
+	menuToggle.classList.toggle("active");
+	openNav();
+});
 
 function openNav() {
-  var width = window.innerWidth;
-  if (width <= 700) {
-    document.getElementById("mySidenav").classList.toggle('side-fullwidth');
-  }
-  else{
-    document.getElementById("mySidenav").classList.toggle('side-width');
-    document.getElementById("main").classList.toggle('margin-250');
-    document.getElementById("navbarColor01").classList.toggle('margin-250');
-  }
-    
-  }
-  var myDiv = document.querySelectorAll(".pic-container");
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-          entry.target.classList.toggle("fadeInTop",entry.isIntersecting)
-          if(entry.isIntersecting) observer.unobserve(entry.target)
-          })
-    },
-{
-  threshold: 0.7,
+	var width = window.innerWidth;
+	if (width <= 700) {
+		document
+			.getElementById("mySidenav")
+			.classList.toggle("side-fullwidth");
+	} else {
+		document.getElementById("mySidenav").classList.toggle("side-width");
+		document.getElementById("main").classList.toggle("margin-250");
+		document
+			.getElementById("navbarColor01")
+			.classList.toggle("margin-250");
+	}
 }
-)
-myDiv.forEach(card =>{
-  observer.observe(card);
-})
+var btn = $(".m-play-btn");
+var clip = document.querySelectorAll(".m-clip");
+var firstBtn = $("#firstBtn");
+var firstClip = $(".firstClip");
+var close = $(".m-close");
+var firstVideo = $("#firstVid");
+var video = document.querySelectorAll("video");
+
+$(".m-play-btn").click(function () {
+	let userClickedBtn = $(this).attr("id");
+	if (userClickedBtn == "firstBtn") {
+		clip[0].classList.add("active");
+		video[0].play();
+		video[0].currentTime = 0;
+	} else if (userClickedBtn == "secBtn") {
+		clip[1].classList.add("active");
+		video[1].play();
+		video[1].currentTime = 0;
+	} else if (userClickedBtn == "thirdBtn") {
+		clip[2].classList.add("active");
+		video[2].play();
+		video[2].currentTime = 0;
+	}
+	console.log(userClickedBtn);
+});
+
+close.click(function () {
+	for (let i = 0; i < 3; i++) {
+		clip[i].classList.remove("active");
+		video[i].pause();
+	}
+});
+var myDiv = document.querySelectorAll(".pic-container");
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			entry.target.classList.toggle("fadeInTop", entry.isIntersecting);
+			if (entry.isIntersecting) observer.unobserve(entry.target);
+		});
+	},
+	{
+		threshold: 0.7,
+	}
+);
+myDiv.forEach((card) => {
+	observer.observe(card);
+});
--- js/login.js
+++ js/login.js
@@ -1,41 +1,51 @@
 window.onload = function () {
-
-    function validate(e) {
-        var fnameDiv = document.getElementById('fName')
-        var firstName = document.getElementById('firstName');
-        if (firstName.value.trim() == '') {
-            // Add a class that defines an animation
-            fnameDiv.classList.add('shake-horizontal');
-            // remove the class after the animation completes
-            setTimeout(function() {
-                fnameDiv.classList.remove('shake-horizontal');
-            }, 800);
-            e.preventDefault();
-        }
-        var passDiv = document.getElementById('pass');
-        var password = document.getElementById('password');
-        if (password.value.trim() == '') {
-            
-            // Add a class that defines an animation
-            passDiv.classList.add('shake-horizontal');
-            // remove the class after the animation completes
-            setTimeout(function() {
-                passDiv.classList.remove('shake-horizontal');
-            }, 800);
-            e.preventDefault();
-        }
-        var emailDiv = document.getElementById('Email');
-        var email = document.getElementById('email');
-        if (email.value.trim() == '') {
-            
-            // Add a class that defines an animation
-            emailDiv.classList.add('shake-horizontal');
-            // remove the class after the animation completes
-            setTimeout(function() {
-                emailDiv.classList.remove('shake-horizontal');
-            }, 800);
-            e.preventDefault();
-        }
-    }
-    document.getElementById('login').addEventListener('submit', validate);
-}
+	function validate(e) {
+		let warningText = $(".warning");
+		var fnameDiv = document.getElementById("fName");
+		var firstName = document.getElementById("firstName");
+		if (firstName.value.trim() == "") {
+			warningText.addClass("error");
+			// Add a class that defines an animation
+			fnameDiv.classList.add("shake-horizontal");
+			// remove the class after the animation completes
+			setTimeout(function () {
+				fnameDiv.classList.remove("shake-horizontal");
+			}, 800);
+			setTimeout(() => {
+				warningText.removeClass("error");
+			}, 4000);
+			e.preventDefault();
+		}
+		var passDiv = document.getElementById("pass");
+		var password = document.getElementById("password");
+		if (password.value.trim() == "") {
+			warningText.addClass("error");
+			// Add a class that defines an animation
+			passDiv.classList.add("shake-horizontal");
+			// remove the class after the animation completes
+			setTimeout(function () {
+				passDiv.classList.remove("shake-horizontal");
+			}, 800);
+			setTimeout(() => {
+				warningText.removeClass("error");
+			}, 4000);
+			e.preventDefault();
+		}
+		var emailDiv = document.getElementById("Email");
+		var email = document.getElementById("email");
+		if (email.value.trim() == "") {
+			warningText.addClass("error");
+			// Add a class that defines an animation
+			emailDiv.classList.add("shake-horizontal");
+			// remove the class after the animation completes
+			setTimeout(function () {
+				emailDiv.classList.remove("shake-horizontal");
+			}, 800);
+			setTimeout(() => {
+				warningText.removeClass("error");
+			}, 4000);
+			e.preventDefault();
+		}
+	}
+	document.getElementById("login").addEventListener("submit", validate);
+};
