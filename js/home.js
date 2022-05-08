8d2ab1dc5a4660e843667f48d9d9eee17767b405
N1ghtHunter <93228608+N1ghtHunter@users.noreply.github.com>
2022-05-08 22:45:31 +0200

    5/8 Files upload mazin

--- js/home.js
+++ js/home.js
@@ -66,3 +66,79 @@ const observer = new IntersectionObserver(
 myDiv.forEach((card) => {
 	observer.observe(card);
 });
+// search bar
+
+const searchInput = document.querySelector("[data-search]");
+let movies = [];
+const names = document.querySelectorAll("#searchResult li");
+
+window.onload = function () {
+	const searchResult = document.querySelector("#searchResult");
+	document.onclick = function (event) {
+		if (
+			event.target.id == "search-input" ||
+			event.target.id == "search-btn"
+		) {
+			console.log("block");
+			searchResult.style.display = "block";
+		} else if (
+			event.target.id !== "searchResult" &&
+			event.target.id !== "search-input" &&
+			event.target.id !== "search-btn"
+		) {
+			console.log("none");
+			searchResult.style.display = "none";
+		}
+	};
+};
+
+document.getElementById("search-btn").addEventListener("click", findMyDiv);
+searchInput.addEventListener("input", (e) => {
+	const value = e.target.value;
+	console.log(value);
+	movies.forEach((movie) => {
+		const isVisible = movie;
+	});
+});
+function closeAll() {
+	for (let i = 0; i < names.length; i++) {
+		if (names[i]) names[i].style.display = "none";
+	}
+}
+function findMyDiv() {
+	closeAll();
+	var str_needle = searchInput.value;
+	console.log(str_needle);
+	str_needle = str_needle.toUpperCase();
+	if (str_needle != "") {
+		for (let i = 0; i < names.length - 1; i++) {
+			if (names[i]) {
+				var str_haystack = names[i].innerHTML.toUpperCase();
+
+				if (str_haystack.includes(str_needle)) {
+					console.log(str_haystack);
+					names[i].style.display = "block";
+				} else {
+					document.getElementById("no-result").style.display =
+						"block";
+				}
+			}
+		}
+	}
+}
+const toggle = document.getElementById("themetoggle");
+const body = document.querySelector("body");
+const footerBtn = document.querySelectorAll("footer .btn-outline-light");
+const textChanger = document.querySelectorAll(".text-white");
+toggle.onclick = () => {
+	toggle.classList.toggle("theme-active");
+	body.classList.toggle("theme-active");
+	for (let i = 0; i < footerBtn.length; i++) {
+		footerBtn[i].classList.toggle("btn-outline-light");
+		footerBtn[i].classList.toggle("btn-outline-dark");
+	}
+	for (let i = 0; i < textChanger.length; i++) {
+		textChanger[i].classList.toggle("text-white");
+		textChanger[i].classList.toggle("text-dark");
+	}
+};
--- js/login.js
+++ js/login.js
@@ -1,8 +1,27 @@
 window.onload = function () {
 	function validate(e) {
 		let warningText = $(".warning");
-		var fnameDiv = document.getElementById("fName");
-		var firstName = document.getElementById("firstName");
+		const fnameDiv = document.getElementById("fName");
+		const firstName = document.getElementById("firstName");
+		const emailDiv = document.getElementById("Email");
+		const email = document.getElementById("email");
+		const passDiv = document.getElementById("pass");
+		const password = document.getElementById("password");
+		const paswd =
+			/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+		if (email.value.trim() == "") {
+			warningText.addClass("error");
+			// Add a class that defines an animation
+			emailDiv.classList.add("shake-horizontal");
+			// remove the class after the animation completes
+			setTimeout(function () {
+				emailDiv.classList.remove("shake-horizontal");
+			}, 800);
+			setTimeout(() => {
+				warningText.removeClass("error");
+			}, 4000);
+			e.preventDefault();
+		}
 		if (firstName.value.trim() == "") {
 			warningText.addClass("error");
 			// Add a class that defines an animation
@@ -16,13 +35,9 @@ window.onload = function () {
 			}, 4000);
 			e.preventDefault();
 		}
-		var passDiv = document.getElementById("pass");
-		var password = document.getElementById("password");
 		if (password.value.trim() == "") {
 			warningText.addClass("error");
-			// Add a class that defines an animation
 			passDiv.classList.add("shake-horizontal");
-			// remove the class after the animation completes
 			setTimeout(function () {
 				passDiv.classList.remove("shake-horizontal");
 			}, 800);
@@ -31,18 +46,14 @@ window.onload = function () {
 			}, 4000);
 			e.preventDefault();
 		}
-		var emailDiv = document.getElementById("Email");
-		var email = document.getElementById("email");
-		if (email.value.trim() == "") {
-			warningText.addClass("error");
-			// Add a class that defines an animation
-			emailDiv.classList.add("shake-horizontal");
-			// remove the class after the animation completes
-			setTimeout(function () {
-				emailDiv.classList.remove("shake-horizontal");
-			}, 800);
+		if (password.value.match(paswd)) {
+			console.log(done);
+		} else {
+			document.querySelector(".tooltips").classList.add("wrong-pass");
 			setTimeout(() => {
-				warningText.removeClass("error");
+				document
+					.querySelector(".tooltips")
+					.classList.remove("wrong-pass");
 			}, 4000);
 			e.preventDefault();
 		}
