2287610b5976c6ce94b3984433b01e84f9e0bb45
MarinaAymanHanna <103747847+MarinaAymanHanna@users.noreply.github.com>
2022-05-08 23:41:39 +0200

    Add files via upload

---
+++ JS/first-page.js
@@ -0,0 +1,87 @@
+const menuToggle = document.querySelector(".menuToggle");
+menuToggle.addEventListener("click", function () {
+	menuToggle.classList.toggle("active");
+	openNav();
+});
+
+function openNav() {
+	var width = window.innerWidth;
+	if (width <= 700) {
+		document.getElementById("mySidenav").classList.toggle("side-fullwidth");
+	} else {
+		document.getElementById("mySidenav").classList.toggle("side-width");
+		document.getElementById("main").classList.toggle("margin-250");
+		document.getElementById("navbarColor01").classList.toggle("margin-250");
+	}
+}
+
+// search bar
+
+const searchInput = document.querySelector("[data-search]");
+let movies = [];
+const names = document.querySelectorAll("#searchResult li");
+
+window.onload = function () {
+	const searchResult = document.querySelector("#searchResult");
+	document.onclick = function (event) {
+		if (event.target.id == "search-input" || event.target.id == "search-btn") {
+			console.log("block");
+			searchResult.style.display = "block";
+		} else if (event.target.id !== "searchResult" && event.target.id !== "search-input" && event.target.id !== "search-btn") {
+			console.log("none");
+			searchResult.style.display = "none";
+		}
+	};
+};
+
+document.getElementById("search-btn").addEventListener("click", findMyDiv);
+searchInput.addEventListener("input", (e) => {
+	const value = e.target.value;
+	console.log(value);
+	movies.forEach((movie) => {
+		const isVisible = movie;
+	});
+});
+function closeAll() {
+	for (let i = 0; i < names.length; i++) {
+		if (names[i]) names[i].style.display = "none";
+	}
+}
+function findMyDiv() {
+	closeAll();
+	var str_needle = searchInput.value;
+	console.log(str_needle);
+	str_needle = str_needle.toUpperCase();
+	if (str_needle != "") {
+		for (let i = 0; i < names.length - 1; i++) {
+			if (names[i]) {
+				var str_haystack = names[i].innerHTML.toUpperCase();
+
+				if (str_haystack.includes(str_needle)) {
+					console.log(str_haystack);
+					names[i].style.display = "block";
+				} else {
+					document.getElementById("no-result").style.display = "block";
+				}
+			}
+		}
+	}
+}
+
+// theme
+const toggle = document.getElementById("themetoggle");
+const body = document.querySelector("body");
+const footerBtn = document.querySelectorAll("footer .btn-outline-light");
+const textChanger = document.querySelectorAll(".text-white");
+toggle.onclick = () => {
+	toggle.classList.toggle("theme-active");
+	body.classList.toggle("theme-active");
+	for (let i = 0; i < footerBtn.length; i++) {
+		footerBtn[i].classList.toggle("btn-outline-light");
+		footerBtn[i].classList.toggle("btn-outline-dark");
+	}
+	for (let i = 0; i < textChanger.length; i++) {
+		textChanger[i].classList.toggle("text-white");
+		textChanger[i].classList.toggle("text-dark");
+	}
+};
